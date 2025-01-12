b792c8e22f2c055933906871561cd808c25cdf38
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2025-01-12 20:27:46 -0300

    fix: update modal component

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -31,7 +31,7 @@
 </div>
 @push('scripts')
     <script>
-        function activeInactive(route, label, register, method = 'DELETE') {
+        function modalDelete(route, label, register, method = 'DELETE') {
             // Atualizar mensagens
             $('#msg_inactive').hide();
             if (label === 'inactive' || label === 'deletar') {
@@ -57,14 +57,11 @@
             var modal = document.getElementById('id01');
             modal.style.display = 'block';
 
-            // When the user clicks anywhere outside of the modal, close it
             window.onclick = function(event) {
                 if (event.target == modal) {
                     modal.style.display = "none";
                 }
             }
-
-
         }
     </script>
 @endpush
