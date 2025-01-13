a9cff24bc50626510176bf599bddaf33b98954a1
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2025-01-13 08:47:49 -0300

    fix: update baseService

--- app/Services/BaseService.php
+++ app/Services/BaseService.php
@@ -35,30 +35,4 @@ class BaseService
             throw $e;
         }
     }
-
-    public function buildQuery($model)
-    {
-        $loggedUser = Auth::id();
-        $managerId = User::where('id', $loggedUser)->value('manager_id');
-        $query = $model::query();
-        if ($managerId === null) {
-            $query->where(function ($q) use ($loggedUser) {
-                $q->where('user_id', $loggedUser)
-                    ->orWhereIn('user_id', function ($subQuery) use ($loggedUser) {
-                        $subQuery->select('id')->from('users')->where('manager_id', $loggedUser);
-                    });
-            });
-        } else {
-            if (method_exists($model, 'users')) {
-                $query->with('users')->where(function ($q) use ($loggedUser) {
-                    $q->where('user_id', $loggedUser)->orWhereHas('users', function ($subQuery) use ($loggedUser) {
-                        $subQuery->where('users.id', $loggedUser);
-                    });
-                });
-            } else {
-                $query->where('user_id', $loggedUser);
-            }
-        }
-        return $query;
-    }
 }
